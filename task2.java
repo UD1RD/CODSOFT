c02818d4d20bf39d0013bf5f096c60c5488a2699
UD1RD <rudra49827@gmail.com>
2024-06-10 12:53:31 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+        
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+        
+        double averagePercentage = (double) totalMarks / numSubjects;
+        String grade = calculateGrade(averagePercentage);
+        
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks + " out of " + (numSubjects * 100));
+        System.out.println("Average Percentage: " + String.format("%.2f", averagePercentage) + "%");
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+    
+    private static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A";
+        } else if (averagePercentage >= 80) {
+            return "B";
+        } else if (averagePercentage >= 70) {
+            return "C";
+        } else if (averagePercentage >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
