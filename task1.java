6bcdb10c0b4d0c3e2044159b16d104cb93be89df
UD1RD <rudra49827@gmail.com>
2024-06-10 12:50:45 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean playAgain = true;
+        int score = 0;
+
+        System.out.println("Welcome to the Guessing Game!");
+
+        while (playAgain) {
+            int numberToGuess = random.nextInt(100) + 1;
+            int attempts = 0;
+            boolean hasGuessedCorrectly = false;
+
+            System.out.println("A new number has been generated. Try to guess it!");
+
+            while (attempts < 10 && !hasGuessedCorrectly) {
+                System.out.print("Enter your guess (1 to 100): ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == numberToGuess) {
+                    System.out.println("Congratulations! You guessed the correct number.");
+                    hasGuessedCorrectly = true;
+                    score++;
+                } else if (userGuess < numberToGuess) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (!hasGuessedCorrectly) {
+                System.out.println("You've used all attempts. The number was: " + numberToGuess);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String userResponse = scanner.next();
+            playAgain = userResponse.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Game over! Your score: " + score);
+        scanner.close();
+    }
+}
